c239d9a1f314a123578eaae5f552de5498cf47a2
Diacklin Fabiana Aular <74654266+dfaby-aular@users.noreply.github.com>
2023-04-03 11:30:04 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+    
+    </div>
+  )
+}
+
+export default App
